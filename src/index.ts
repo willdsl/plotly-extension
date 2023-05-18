8afa7fe3a432da7082e09a1c4dbd7f7a0120d3e3
William Li <william.dsl@gmail.com>
2023-05-18 15:26:00 -0700

    adding command to show dialog with user input and confirmation message

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,40 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, InputDialog, showDialog } from '@jupyterlab/apputils';
+
 /**
  * Initialization data for the plotly-extension extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'plotly-extension:plugin',
+  id: 'plotly-extension',
   description: 'plotly take home assignment',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension plotly-extension is activated!');
+    console.log('ICommandPalette:', palette);
+
+    const command: string = 'plotly-extension:open-dialog';
+    app.commands.addCommand(command, {
+      label: 'Open dialog',
+      execute: async () => {
+        const input = await InputDialog.getText({
+          title: 'Please enter text input',
+          okLabel: 'OK',
+          placeholder: 'Placeholder text',
+        });
+
+        if (input.button.accept) {
+          showDialog({
+            title: 'You\'ve entered the following:',
+            body: input.value?.toString(),
+          });
+        }
+      }
+    });
+
+    palette.addItem({ command, category: 'PlotlyAssessment' });
   }
 };
 
